c8c04433ad36a9389d0af6a40364460472c409a9
Shubham Kumar <98862179+shubham21155102@users.noreply.github.com>
2022-03-06 23:46:44 +0530

    Add files via upload

---
+++ iitbhu_logo_shubham.py
@@ -0,0 +1,23 @@
+from turtle import *
+goto(-250,0)
+bgcolor("black")
+color("red")
+speed(0)
+#begin_fill()
+points=1
+while points<170:
+    forward(450)
+    left(145)
+    points =points+1
+end_fill()
+goto(-160,-220)
+color("Purple")
+write("IIT BHU",font=("Helvetica",40,"bold"))
+color("Purple")
+
+goto(-240,-240)
+
+write("(BANARAS HINDU UNIVERSITY)",font=("Itallic",20,"bold"))
+speed(1)
+color("black")
+bk(2000)
